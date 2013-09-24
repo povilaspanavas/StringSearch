4018eeb932aa5ba7c07e73163eeb42f873ad36cf
Povilas Panavas <povilas.panavas@gmail.com>
2013-09-24 18:42:52 +0100

    Adding assert statements

---
+++ TestFramework/Engine/Assert.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PUnit.Framework
+{
+    public class Assert
+    {
+        private const string AssertFailed = "Assert failed.";
+        private const string StandardFormat = "{0} Expected {1}, but was {2}.";
+
+        public static void AreEqual(int expected, int actual)
+        {
+            if (expected.Equals(actual) == false)
+                throw new AssertException(string.Format(StandardFormat, 
+                    Assert.AssertFailed, expected, actual));
+        }
+
+        public static void IsTrue(bool val)
+        {
+            if (val == false)
+                throw new AssertException(string.Format(StandardFormat,  
+                    Assert.AssertFailed, true, val));
+        }
+    }
+}
---
+++ TestFramework/Engine/AssertException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PUnit.Framework
+{
+    public class AssertException : Exception
+    {
+        private string _errorMessage = string.Empty;
+
+        public AssertException() : this(string.Empty)
+        {
+
+        }
+
+        public AssertException(string errorMessage) : base()
+        {
+            this._errorMessage = errorMessage;
+        }
+    }
+}
